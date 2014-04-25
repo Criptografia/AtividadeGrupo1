db0cfb1ffdfb2d007ceb7bfaa3074345adbf62a7
louisamorim <louis.samorim@gmail.com>
2014-04-25 14:00:04 -0300

    substituição cripto e descripto

--- criptografaSimples.cpp
+++ criptografaSimples.cpp
@@ -2,22 +2,56 @@
 #include <string>
 using namespace std;
 
-void convertToASCII(string letter)
+string subtituicao(string letter,string chave)
 {
+    int aux=23;
     for (int i = 0; i < letter.length(); i++)
     {
         char x = letter.at(i);
-        cout << int(x) << endl;
+        x= int(x+chave[i%chave.length()]+aux);
+        while(int(x)>=255 ){
+                      x-=255;}
+        
+        aux=int(letter.at(i));
+        letter.at(i)=x;
     }
+    
+       return letter;
 }
 
 
+string descripto(string letter,string chave)
+{
+    int aux=23;
+    for (int i = 0; i < letter.length(); i++)
+    {
+        char x = letter.at(i);
+        x= int(x-chave[i%chave.length()]-aux);
+         while(int(x)<=0 ){
+                      x+=255;
+         }
+   
+        letter.at(i)=x;
+        aux=int(letter.at(i));
+    }
+    
+       return letter;
+}
 
 int main()
 {
-    string plainText;
+
+    string chave, textoClaro, textoCripto;
+    
     cout << "Enter text to convert to ASCII: ";
-    cin >> plainText;
-    convertToASCII(plainText);
+    getline(cin, textoClaro);
+    cout << "Digite a chave de criptografia: ";
+    cin >> chave;
+
+    textoCripto = subtituicao(textoClaro,chave);
+    cout << textoCripto << endl;
+    textoClaro = descripto(textoCripto,chave);
+    cout << textoClaro << endl;
+    cin >>chave ;
     return 0;
 }
