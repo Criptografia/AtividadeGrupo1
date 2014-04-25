859c44fd5bb734af930eb1ca25e77a5ddb5208f6
louisamorim <louis.samorim@gmail.com>
2014-04-25 19:24:34 -0300

    Update criptografaSimples.cpp

--- criptografaSimples.cpp
+++ criptografaSimples.cpp
@@ -9,8 +9,6 @@ string subtituicao(string letter,string chave)
     {
         char x = letter.at(i);
         x= int(x+chave[i%chave.length()]+aux);
-        while(int(x)>=255 ){
-                      x-=255;}
         
         aux=int(letter.at(i));
         letter.at(i)=x;
@@ -20,38 +18,20 @@ string subtituicao(string letter,string chave)
 }
 
 
-string descripto(string letter,string chave)
-{
-    int aux=23;
-    for (int i = 0; i < letter.length(); i++)
-    {
-        char x = letter.at(i);
-        x= int(x-chave[i%chave.length()]-aux);
-         while(int(x)<=0 ){
-                      x+=255;
-         }
-   
-        letter.at(i)=x;
-        aux=int(letter.at(i));
-    }
-    
-       return letter;
-}
 
 int main()
 {
 
     string chave, textoClaro, textoCripto;
     
-    cout << "Digite o texto para ser criptografado: ";
+    cout << "Digite o texto a ser critografado: ";
     getline(cin, textoClaro);
     cout << "Digite a chave de criptografia: ";
     cin >> chave;
 
     textoCripto = subtituicao(textoClaro,chave);
     cout << textoCripto << endl;
-    textoClaro = descripto(textoCripto,chave);
-    cout << textoClaro << endl;
+    
     cin >>chave ;
     return 0;
 }
