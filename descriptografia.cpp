709ed2430fafaac88ab39d09f9797b218d30580a
louisamorim <louis.samorim@gmail.com>
2014-04-25 19:33:13 -0300

    Create descriptografia.cpp

---
+++ descriptografia.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+string descripto(string letter,string chave)
+{
+    int aux=23;
+    for (int i = 0; i < letter.length(); i++)
+    {
+        char x = letter.at(i);
+        x= int(x-chave[i%chave.length()]-aux);
+   
+        letter.at(i)=x;
+        aux=int(letter.at(i));
+    }
+    
+       return letter;
+}
+
+int main()
+{
+
+    string chave, textoClaro, textoCripto;
+    
+    cout << "Digite o texto critografado:  ";
+    getline(cin, textoCripto);
+    cout << "Digite a chave de criptografia: ";
+    cin >> chave;
+
+    textoClaro = descripto(textoCripto,chave);
+    cout << textoClaro << endl;
+    cin >>chave ;
+    return 0;
+}
