7eb692f88635b516707d8e4e6feff64b05a10077
louisamorim <louis.samorim@gmail.com>
2014-05-05 18:20:41 -0300

    transposição acoplada

--- criptografaSimples.cpp
+++ criptografaSimples.cpp
@@ -54,18 +54,104 @@ string subtituicao(string letter,string chave)
     
        return letter;
 }
+void transposicao (string texto, string key){
+//	cout << "Entrou na Funcao\n";
+//	cout << "A chave e: "<<key <<endl;
+//	cout << "O texto antes da transposicao: " <<texto <<endl;	
+
+
+	//TRANSPOSICAO
+
+	int tam = texto.length();
+
+
+
+	//DEFINIR O NUMERO DE COLUNAS
+	int num_col;
+
+	if (tam%8==0){
+		num_col = tam/8;
+	}
+	else  {
+		num_col = (tam/8)+1;
+	}
+
+	char transp[8][num_col];
+//
+
+	for (int i=0; i<tam;i++){
+		if (isspace(texto[i])){
+		texto[i]+=1;
+		}
+		for (int k=1;k<num_col;k++){
+			if (i<k*8){
+				transp[i%8][0] = texto[i];
+				break;
+			}
+			else if (i>=k*8 && i<(k+1)*8){
+				transp[i%8][k] = texto[i];
+				break;
+			}
+
+		}		
+
+
+	}
+
+//	EXIBIR A MATRIZ
+//	for (int i=0;i<8;i++){
+//		for (int j=0;j<num_col;j++){
+//		
+//		if (j == (num_col-1)){
+//		cout << transp[i][j] <<endl;	
+//		}
+//		else
+//		cout << transp[i][j] <<" ";
+//		}
+//	}
+
+
+	//APLICAR A CHAVE
+//	int change = key.length();
+//	char aux[num_col];
+
+//	cout << "Texto depois da transposicao:"; 
+	for (int i=0;i<8;i++){
+		for (int j=0;j<num_col;j++){
+			cout << transp[i][j];
+		}
+		cout <<" ";	
+	}
+
+
+
+}
 
 
 int main()
 {
 
     string chave, textoClaro, textoCripto;
+    int opcao=0;
     cout << "Digite o texto a ser critografado: ";
     getline(cin, textoClaro);
     cout << "Digite a chave de criptografia: ";
     cin >> chave;
+    
+    cout << "Escolha a opcao: "<< endl<<"1 - Substituicao "<< endl <<"2 - Transposicao "<< endl<<"3 - Substituicao e Transposicao "<<endl  ;
+    cin >> opcao;
+      
+    switch(opcao){
+                  case 1:
+                  textoCripto = subtituicao(textoClaro,chave);
+                  break;
+                  case 3:
+                  textoClaro = subtituicao(textoClaro,chave);
+                  case 2:
+                  transposicao(textoClaro,chave);
+                  break;
+    }
 
-    textoCripto = subtituicao(textoClaro,chave);
     cout << textoCripto << endl;
 
 
