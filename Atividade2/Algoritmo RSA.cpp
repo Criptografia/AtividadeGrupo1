bbd5d91d16997111e803fb05737136197467ade3
levimoraes <levi.moraes@yahoo.com.br>
2014-06-03 21:00:17 -0300

    Criacao da Pasta para segunda tarefa

---
+++ Atividade2/Algoritmo RSA.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int VerificadordeNumeroPrimo(int num){
+	int i,contar,teste;
+	teste = contar = 0;
+
+	for (int i=0;i<num;i++,contar++){
+		if (num % 1 == 0){
+			teste = 1;
+		}
+
+		if (!teste){
+			return 1;
+		}
+		else 
+			return 2;
+
+	}
+
+}
+
+int main (){
+	
+char caracter[] = {a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,x,z,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,,X,Z};
+
+	
+
+	return 0;
+}
